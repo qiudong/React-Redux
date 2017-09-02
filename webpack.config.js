fa291010752fc952cb7185e1feceb6e28351df6b
qiudong <qiu_dong@suixinpay.com>
2017-09-02 10:34:18 +0800

    提交

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,21 @@
-import webpack from 'webpack'
+var webpack = require('webpack')
+var path = require('path')
+
+module.exports = {
+  context: __dirname +'/src',
+  entry: "./js/index.js",
+  module: {
+    loaders: [{
+      test:   /\.js?$/,
+      exclude: /(node_modules)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }]
+  },
+  output:{
+    path: __dirname+"/src",
+    filename: "bundle.js",
+  }
+}
